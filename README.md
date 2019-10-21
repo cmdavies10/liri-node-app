54f6a2ef5b23e463b6ce8b199a9f833bbac8e8be
Chris Davies <c.m.davies10@gmail.com>
2019-10-21 17:20:43 -0400

    fully-functional with updated readMe, log.txt

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # liri-node-app
+
+### Overview
+LIRI ('Language Interpretation and Recognition Interface) is a command line node
+app that takes in parameters and gives back data. The app takes in user input
+and returns detailed information on upcoming concerts, songs and movies.
+
+The following commands will take user input and return detailed information on
+upcoming concerts, songs and movies to the terminal:
+1. `concert-this` searches the `Bands in Town Artist Events API and renders the below
+     information about an event to the terminal
+    * Venue Name
+    * Venue City
+    * Event Date
+2. `spotify-this-song` utilizes the `node-spotify-api` package to retrieve the
+   following song information from the Spotify API:
+   * Artist(s)
+   * Song Name
+   * Preview Link from Spotify
+   * Album Name
+3. `movie-this` uses the OMDB API to output the following information on a given
+  movie to the terminal:
+    * Movie Title
+    * Release Year
+    * IMDB Rating
+    * Rotten Tomatoes Rating
+    * Country of Production
+    * Language(s)
+    * Plot
+    * Actor(s) Name(s)
+4. `do-what-it-says` reads text from inside the `random.txt` file and calls one of
+   LIRI's commands
+
+### Instructions
+In the terminal, users enter `node liri.js` followed by one of the following:
+1. `concert-this <artist/band name>`
+2. `spotify-this-song <song name>`
+3. `movie-this <movie name>`
+4. `do-what-says`
+
+
--- liri.js
+++ liri.js
@@ -50,12 +50,6 @@ function concertThis(userInput) {
                 "\n"
             console.log(concertInfo);
 
-            // console.log("===")
-            // console.log("Venue Name: " + response.data[i].venue.name);
-            // console.log("Venue Location: " + response.data[i].venue.city); // ADD STATE OR COUNTRY
-            // console.log("Concert Date: " +
-            // moment(response.data[i].datetime).format("MM/DD/YYYY"));
-
             fs.appendFile("log.txt", concertInfo, function(err) {
                 if (err) {
                     console.log(err);
@@ -101,12 +95,6 @@ function spotifyThis(userInput) {
                 "\n"
             console.log(songInfo);
 
-            // console.log("===")
-            // console.log("Artist(s): " + songs[i].artists[0].name);
-            // console.log("Song Name: " + songs[i].name);
-            // console.log("Song Preview: " + songs[i].preview_url);
-            // console.log("Album: " + songs[i].album.name);
-
             fs.appendFile("log.txt", songInfo, function(err) {
                 if (err) {
                     console.log(err);
@@ -137,16 +125,6 @@ function movieThis(userInput) {
             "\n"
         console.log(movieInfo);
 
-        // console.log("===")
-        // console.log("Title: " + response.data.Title);
-        // console.log("Year: " + response.data.Year);
-        // console.log("IMDB Rating: " + response.data.imdbRating);
-        // console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        // console.log("Country(ies): " + response.data.Country);
-        // console.log("Language(s): " + response.data.Language);
-        // console.log("Plot: " + response.data.Plot);
-        // console.log("Actor(s): " + response.data.Actors);
-
         fs.appendFile("log.txt", movieInfo, function(err) {
             if (err) {
                 console.log(err);
@@ -176,17 +154,8 @@ function doThis(userInput) {
         console.log(dataArr); // REMOVE
 
         userCommand = dataArr[0];
-        console.log(userCommand); // REMOVE
-
         userInput = dataArr[1];
-        console.log(userInput); // REMOVE -- do the quotes need to be removed?
 
         liriBot(userCommand, userInput);
-
-        // fs.appendFile("log.txt", liriBot(userCommand, userInput), function(err) {
-        //     if (err) {
-        //         console.log(err);
-        //     };
-        // });
     });
 };
